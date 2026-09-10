403ff9eddf452ee8ac18fdf0a7f95cbd2e9717f1
isanwicakkk <wicaksonoikhzan@gmail.com>
2026-09-10 19:14:12 +0700

    update dashboard page

--- dashboard/app.py
+++ dashboard/app.py
@@ -37,7 +37,7 @@ st.set_page_config(
 st.markdown("""
     <style>
         .block-container {
-            padding-top: 0rem !important;
+            padding-top: 0.5rem !important;
             padding-bottom: 0rem !important;
         }
     </style>
